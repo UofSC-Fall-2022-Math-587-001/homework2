e34554e0e681ed75da4f98039313d0666bb09665
Matthew Ballard <matt@mrb.email>
2022-08-26 15:06:10 -0400

    fix tests

--- euclid_test.go
+++ euclid_test.go
@@ -13,3 +13,30 @@ func Test1(t *testing.T) {
 	}
 }
 
+func Test2(t *testing.T) {
+	got := gcd(228,1056)[0]
+	want := 12
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func Test3(t *testing.T) {
+	got := gcd(163961,167181)[0]
+	want := 7 
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func Test4(t *testing.T) {
+	got := gcd(3892394,238847)[0]
+	want := 1 
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
